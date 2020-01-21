23c937cfc3b2e56633fae7e11b48ee1acec81816
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-21 12:16:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,11 +14,5 @@ def reverse_each_word(string)
   new_array
 end
 
-def reverse_each_word_collect(string)
-  array = string.split(" ")
-  new_array = []
-  array.collect do |string|
-    new_array << string.reverse
-  end
   new_array
 end
