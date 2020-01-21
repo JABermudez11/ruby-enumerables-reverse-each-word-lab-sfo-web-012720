d0befbdda5fedbeb3f0ff62d7c2c122db811422f
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-21 12:18:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  unchanged = string.split(" ")
-  result = []
-  unchanged.each do |reverse|
-    result << reverse.reverse
-  end
-  result.join(" ")
-end
+#def reverse_each_word(string)
+#  unchanged = string.split(" ")
+#  result = []
+#  unchanged.each do |reverse|
+#    result << reverse.reverse
+#  end
+#  result.join(" ")
+#end
 
 def reverse_each_word_collect(string)
   array = string.split(" ")
